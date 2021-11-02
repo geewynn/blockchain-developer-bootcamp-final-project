4f0353819209546e5896ef45a78806737d89f93e
Geewynn <ekainugodwin@gmail.com>
2021-11-02 04:52:44 +0100

    added main layout component

---
+++ client/src/components/main-layout/main-layout.component.js
@@ -0,0 +1,45 @@
+import { useContext, useEffect } from "react";
+import { AccountContext } from "../contexts/account-context";
+import { FunctionContext } from "../contexts/function-context"
+import { getWeb3 } from "../../utils/getWeb3"
+import Content from "../content/content.component"
+import { MainLayoutWrapper } from "./main-layout.style"
+import SupplyChainContract from "../../abi/SupplyChain.json"
+
+
+
+const MainLayout = () => {
+    const { setAccountDetails } = useContext(AccountContext)
+    const { setContractInstance } = useContext(FunctionContext)
+
+    const enableWeb3 = async () => {
+        try {
+            const web3 = await getWeb3()
+            const accounts = await web3.eth.getAccounts()
+            setAccountDetails(accounts[0])
+
+            const networkId = await web3.eth.net.getId()
+            const deployedNetwork = await SupplyChainContract.networks[networkId]
+            const supplyChain = await new web3.eth.Contract(SupplyChainContract.abi, deployedNetwork && deployedNetwork.address)
+            setContractInstance(supplyChain)
+            console.log('network id', networkId)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        enableWeb3()
+    
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, [])
+    
+
+    return (
+        <MainLayoutWrapper>
+            <Content />
+        </MainLayoutWrapper>
+    )
+}
+
+export default MainLayout
---
+++ client/src/components/main-layout/main-layout.style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const MainLayoutWrapper = styled.main`
+  width: 80vw;
+  height: 80vh;
+  display: flex;
+  background: #fff;
+  // opacity: 0.5;
+  border-radius: 5px;
+  justify-content: center;
+  align-items: center;
+  margin-left: 9vw;
+
+`
+
+
+
+
+export { MainLayoutWrapper }
